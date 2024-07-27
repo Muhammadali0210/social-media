262dc7397ff3df94d01aeddb6d4eaf087eb28c27
Muhammadali0210 <theprogrammer0210@gmail.com>
2024-07-27 19:45:28 +0500

    some tag proble resolved

--- pages/create.vue
+++ pages/create.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 
 const state = reactive({
   email: undefined,
-  password: undefined
-})
+  password: undefined,
+});
 </script>
 
 <template>
-    <div class="h-screen p-0">
-        <UiNavbar name="Create" />
-        <dir class="w-full h-full flex justify-center sm:p-10 sm:pb-40   sm:items-center max-sm:items-start p-0 m-0 max-sm:translate-y-[-10px]">
-            <UiAddCard />
-        </dir>
+  <div class="h-screen">
+    <UiNavbar name="Create" />
+    <div
+      class="w-full h-full flex justify-center sm:p-10 sm:pb-40 sm:items-center max-sm:items-start p-0 m-0 max-sm:translate-y-[-5px]"
+    >
+      <UiAddCard />
     </div>
+  </div>
 </template>
--- pages/profile.vue
+++ pages/profile.vue
@@ -9,7 +9,7 @@ import { profileTabs } from '~/const/const';
         <section class="w-full h-[90vh] flex flex-col items-start overflow-y-auto"> 
 
 
-            <dir class="border-b border-slate-200 dark:border-slate-600 bg-white dark:bg-gray-900 w-full p-5 m-0 flex justify-start gap-3">
+            <div class="border-b border-slate-200 dark:border-slate-600 bg-white dark:bg-gray-900 w-full p-5 m-0 flex justify-start gap-3">
 
                 <div class="w-[120px] h-[120px] overflow-hidden"  style="border-radius: 100%; border: 3px solid green;">
                     <div class="w-full h-full overflow-hidden" style="border-radius: 100%; border: 3px solid white;">
@@ -19,15 +19,15 @@ import { profileTabs } from '~/const/const';
                     </div>
                 </div>
 
-                <dir class="text-wrap">
+                <div class="text-wrap">
                     <div class="text-xl font-bold">Muhammadali</div>
                     <div class="text-sm text-black dark:text-gray-400">@muhammadali0210</div> 
                     <NuxtLink to="/edit-profile">
                         <UButton  color="gray" variant="outline" class="text-sm mt-2 dark:hover:bg-inherit  dark:hover:text-white hover:text-black" >Edit profile</UButton>
                     </NuxtLink>
-                </dir>
+                </div>
                 
-            </dir>
+            </div>
             <div class="w-full mt-0">
                 <UTabs :items="profileTabs" class="w-full mt-0 border-none">
                     <template #icon="{ item, selected }">
